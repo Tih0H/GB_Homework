1941b9a4711728147a861af2484cffb57d622cbf
Tih0H <47225385+Tih0H@users.noreply.github.com>
2022-03-05 23:58:09 +0300

    Подправил JS

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,20 +1,20 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
-  el: '#app',
+  el: '#app',//подключаем нашь вью и связываем с нашей разметкой
   data: {
-    searchLine : '',
-    isVisibleCart: false,
-    cartUrl: '/getBasket.json',
-    catalogUrl: '/catalogData.json',
-    products: [],
-    cartItems: [],
-    filtered: [],
+    searchLine : '',//создаем пустой обьект для поисковой строки
+    isVisibleCart: false,//Создаем правило для скрытия/прорисовки корзины
+    cartUrl: '/getBasket.json',//урл корзины
+    catalogUrl: '/catalogData.json',//урл товаров
+    products: [],//создаем пустой массив для наших карточек товаров
+    cartItems: [],//создаем пустой массив для корзины
+    filtered: [],//создаем массив для поиска
     imgCatalog: 'https://via.placeholder.com/200x150',
     imgCart: 'https://via.placeholder.com/50x100',
   },
   methods: {
-    getJson(url) {
+    getJson(url) {//создаем логику получения ответа с сервера
       return fetch(url)
         .then(result => result.json())
         .catch(error => {
@@ -22,53 +22,53 @@ const app = new Vue({
           // error trigger
         })
     },
-    addProduct(product) {
-      this.getJson(`${API}/addToBasket.json`)
+    addProduct(product) {//логика добавления продукта в корзину по клику на продукт
+      this.getJson(`${API}/addToBasket.json`)//запрашиваем сервер на добавление в корзину
         .then(data => {
           if (data.result === 1) {
-            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            let find = this.cartItems.find(el => el.id_product === product.id_product);//ищем продукт в массиве продуктов сравнивая с тем на который кликнули по id
             if (find) {
-              find.quantity++;
-            } else {
-              let prod = Object.assign({quantity: 1}, product);
-              this.cartItems.push(prod)
+              find.quantity++;//увиличиваем колличество товара
+            } else {//если это первый товар в корзине то
+              let prod = Object.assign({quantity: 1}, product);//создаем новый массив с данными продукта + добовляем счетчик
+              this.cartItems.push(prod)//пушим в массив корзины
             }
-          } else {
+          } else {//если сервер не выдал положительного ответа - выводим ошибку
             alert('Error');
           }
         })
     },
-    remove(item) {
+    remove(item) {//логика удаления товара из корзины по клику
       this.getJson(`${API}/deleteFromBasket.json`)
-        .then(data => {
+        .then(data => {//делаем запрос на сервер об удалении
           if (data.result === 1) {
-            if (item.quantity > 1) {
+            if (item.quantity > 1) {//при положительном ответе сначала уменьшаем счетчик товара
               item.quantity--;
-            } else {
+            } else {//если только один товар в корзине - полностью удаляем карточку методом splice из массива для корзины cartItems найдя его позицию методом indexOf
               this.cartItems.splice(this.cartItems.indexOf(item), 1)
             }
+          }else {//если сервер не выдал положительного ответа - выводим ошибку
+            alert('Error');
           }
         })
     },
     FilterGoods() {
-      let regexp = new RegExp(this.searchLine , 'i');
+      let regexp = new RegExp(this.searchLine , 'i');//фильтр товара регуляркой
+      console.log('regexp: ', regexp);
       this.filtered = this.products.filter(el => regexp.test(el.product_name));
     },
   },
-  created() {
-    this.getJson(`${API + this.cartUrl}`)
-        .then(data => {
-          for (let el of data.contents) {
-            this.cartItems.push(el);
-          }
-        });
-    this.getJson(`${API + this.catalogUrl}`)
+  created() {//метод создания вью
+    this.getJson(`${API + this.catalogUrl}`)//создаем каталог
         .then(data => {
           for (let el of data) {
-            this.products.push(el);
-            this.filtered.push(el);
+            this.products.push(el);//что получили по запросу с сервера пушим в наш каталог
+            this.filtered.push(el);//что получили по запросу с сервера пушим в наш поиск
           }
         });
   },
 });
 
+const str = 'Geek from Geekbrains';
+const regexp = /(?:)/;
+console.log('str.match(regexp: ', str.match(regexp));
