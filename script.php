1ce21965a32ba0754da0c9ddd373ff3b2a3aaec1
Tih0H <47225385+Tih0H@users.noreply.github.com>
2022-09-28 22:08:56 +0300

    Добавил 1е задание

--- script.php
+++ script.php
@@ -1,4 +1,11 @@
 <?php
-//заглушка
-
+//задание 1
+$arrey1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$arrey2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+$arreyAns = [];
+for ($index = 0; $index < count($arrey1); $index++) {
+    array_push($arreyAns, $arrey1[$index] + $arrey2[$index]);
+}
+print_r($arreyAns);
+//задание 2
 ?>
