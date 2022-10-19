9551592901dc1eae7cb9462d3a981d1508d30dac
Tih0H <47225385+Tih0H@users.noreply.github.com>
2022-10-19 21:15:29 +0300

    Добавил свое 7е дз
    Никак не могу понять почему он мне выдает Uncaught PDOException: SQLSTATE[HY000]

---
+++ model/User.php
@@ -0,0 +1,57 @@
+<?php
+
+class User
+{
+    private int $id;
+    private string $username;
+    private string $name; // Добавляем свойство для имени
+
+    public function __construct(string $username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+// Сделаем методы получения и установки свойств
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+
+
+}
