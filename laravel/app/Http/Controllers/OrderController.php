24363d4982aebe2616ad1febee256c3d2c75086d
Tih0H <47225385+Tih0H@users.noreply.github.com>
2023-04-04 19:08:38 +0300

    ДЗ 4

---
+++ laravel/app/Http/Controllers/OrderController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \view('order.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return \view('order.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'info' => 'required',
+        ]);
+        $file = 'order/' . fake()->uuid() . '_order.txt';
+        $message = 'name: ' . $request->input('name') . PHP_EOL . 'phone: ' . $request->input('phone') . PHP_EOL . 'email: ' . $request->input('email') . PHP_EOL . 'info: ' . $request->input('info') . PHP_EOL;
+        file_put_contents($file, $message, FILE_APPEND);
+        return  response()->json($request->only(['name','phone', 'email','info']));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
